ba80e0ffc83908911551e8bce91995e039e2f3ec
jang inwoo <inuj03@naver.com>
2025-09-11 01:50:45 +0900

    init

---
+++ src/test/java/com/inwoo/project/shopback/ShopBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inwoo.project.shopback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
